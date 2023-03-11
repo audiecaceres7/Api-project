da7ecfcbb7bdbfc16af8448cf7601df1755e6205
Audie Caceres <audiecaceres@Audies-MBP.hsd1.fl.comcast.net>
2023-03-11 15:32:23 -0500

    finish project

--- ts/src/dynamicDataList.ts
+++ ts/src/dynamicDataList.ts
@@ -2,6 +2,6 @@ import { pokemon } from "./fetch";
 
 export function filterTypesOfPokemon(array: pokemon[], type: string) {
     const list = array.filter(elm => elm.type === `${type}`);
-     const value = document.querySelector('.value-container')! as HTMLElement;
+     const value = document.querySelector('.type-info')! as HTMLElement;
      value.innerHTML += `<h2>${type}: ${list.length}</h2>`;
  }
--- ts/src/main.ts
+++ ts/src/main.ts
@@ -9,16 +9,18 @@ const dataSwitchers = document.querySelectorAll('[data-switcher]');
 for (let i = 0; i < dataSwitchers.length; i++) {
     const tabSwitcher = dataSwitchers[i];
     if (tabSwitcher instanceof HTMLElement) {
-        const pageId = tabSwitcher.dataset.tab;
-        tabSwitcher.addEventListener('click', function() {
-            document.querySelector('.nav-items .box.is-active')?.classList.remove('is-active');
-            tabSwitcher.parentElement?.classList.add('is-active');
-            switchPage(pageId);
-        });
+        const pageId = tabSwitcher.dataset.tab; 
+        if (pageId) {
+            tabSwitcher.addEventListener('click', function() {
+                document.querySelector('.nav-items .box.is-active')?.classList.remove('is-active');
+                tabSwitcher.classList.add('is-active');
+                switchPage(pageId);
+            });
+        }
     };
 };
 
-function switchPage(pageId: string | undefined) {
+function switchPage(pageId: string) {
     const currPage = document.querySelector('.container.is-active');
     currPage?.classList.remove('is-active');
 
@@ -54,6 +56,11 @@ gettingPokemonData()
         throw new Error('fav its not defined...');
     }
 
+    const typeList = ['electric', 'water', 'grass', 'bug', 'fire'];
+    for (let i = 0; i < typeList.length; i++) {
+        filterTypesOfPokemon(collection, typeList[i]);
+    };
+
     addingEventListener();
 
     function addingEventListener() {
@@ -76,7 +83,7 @@ gettingPokemonData()
                 }
             })
         }
-        
+
         const favCards = document.querySelectorAll('.fav-card');
         for (const card of favCards) {
             const disLikeBtn = card.querySelector('.fa-trash');
@@ -97,46 +104,51 @@ gettingPokemonData()
         }
     }
     
-    const sortBtn = document.querySelectorAll('.atoz');
-    for (const btn of sortBtn) {
-        btn.addEventListener('click', function() {
-            const colSorted = collection.sort((a, z) => a.name.localeCompare(z.name));
-            const favSorted = favorites.sort((a, z) => a.name.localeCompare(z.name));
-            col.innerHTML = '';
-            fav.innerHTML = '';
+    function sortPokemon(order: 'atoz' | 'ztoa') {
+        let colSorted, favSorted;
+        const col = document.querySelector('.collection-container');
+        if (col === null) {
+            throw new Error('col is not defined...');
+        } 
+        const fav = document.querySelector('.favorites-container');
+        if (fav === null) {
+            throw new Error('fav its not defined...');
+        }
+        if (order === 'atoz') {
+          colSorted = collection.sort((a, z) => a.name.localeCompare(z.name));
+          favSorted = favorites.sort((a, z) => a.name.localeCompare(z.name));
+        } else if (order === 'ztoa'){
+          colSorted = collection.sort((a, z) => z.name.localeCompare(a.name));
+          favSorted = favorites.sort((a, z) => z.name.localeCompare(a.name));
+        }
+        col.innerHTML = '';
+        fav.innerHTML = '';
+        if (colSorted) {
             colSorted.map(pokemon => renderCard(pokemon));
+        }
+        if (favSorted) {
             favSorted.map(pokemon => renderFavCard(pokemon));
-            addingEventListener();
-        })
-    }
-
-    const sortBtnTwo = document.querySelectorAll('.ztoa');
-    for (const btn of sortBtnTwo) {
-        btn.addEventListener('click', function() {
-            const colSorted = collection.sort((a, z) => z.name.localeCompare(a.name));
-            const favSorted = favorites.sort((a, z) => z.name.localeCompare(a.name));
-            col.innerHTML = '';
-            fav.innerHTML = '';
-            colSorted.map(pokemon => renderCard(pokemon));
-            favSorted.map(pokemon => renderFavCard(pokemon));
-            addingEventListener();
-        })
+        }
+        addingEventListener();
     }
     
-
+    const sortBtn = document.querySelectorAll('.atoz');
+    sortBtn.forEach(btn => {
+        btn.addEventListener('click', () => {
+            sortPokemon('atoz');
+        });
+    });
+    
+    const sortBtnTwo = document.querySelectorAll('.ztoa');
+    sortBtnTwo.forEach(btn => {
+        btn.addEventListener('click', () => {
+            sortPokemon('ztoa');
+        });
+    });
 });
 
-// const typeList = ['electric', 'water', 'grass', 'bug', 'fire'];
-// for (let i = 0; i < typeList.length; i++) {
-//     filterTypesOfPokemon(collection, typeList[i]);
-// };
 
-// function sortingToggle(elm: any, selector: string) {
-//     if (document.querySelector(`${selector}.active`) !== null) {
-//         document.querySelector(`${selector}.active`)?.classList.remove('active');
-//     }
-//     elm.classList.add('active');
-// }
+
 
 
 
